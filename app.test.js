61e3bd6326bdf62912022251468befa43726ad23
jaime <jaime.molina@mattersupply.co>
2020-03-21 05:43:34 -0500

    refactor

--- app.test.js
+++ app.test.js
@@ -1,31 +1,35 @@
-const request = require('supertest');
-process.env.DATABASE_URL = "mongodb://localhost/auth-test-test";
-const mongoose = require("mongoose");
-const app = require('./app');
-const User = require("./user");
+require('dotenv').config()
+
+const request = require('supertest')
+process.env.DATABASE_URL = 'mongodb://localhost/auth-test-test'
+const mongoose = require('mongoose')
+const app = require('./app')
+const User = require('./user')
 
 beforeEach(async () => {
-  await User.remove({});
-});
+  await User.remove({})
+})
 
 afterAll(async () => {
-  await mongoose.disconnect();
-});
+  await mongoose.disconnect()
+})
 
 test('GET / responds with success code', async () => {
-  const response = await request(app).get('/');
-  expect(response.statusCode).toBe(302);
-});
+  const response = await request(app).get('/')
+  expect(response.statusCode).toBe(302)
+})
 
 test('POST /register redirects to login', async () => {
-  const response = await request(app).post('/register')
-          .send("username=user1&password=test1234");
-  expect(response.statusCode).toBe(302);
-});
+  const response = await request(app)
+    .post('/register')
+    .send('username=user1&password=test1234')
+  expect(response.statusCode).toBe(302)
+})
 
 test('POST /login redirects to home', async () => {
-  await User.create({ username: "user1", password: "test1234" });
-  const response = await request(app).post('/login')
-          .send("username=user1&password=test1234");
-  expect(response.statusCode).toBe(302);
-});
+  await User.create({ username: 'user1', password: 'test1234' })
+  const response = await request(app)
+    .post('/login')
+    .send('username=user1&password=test1234')
+  expect(response.statusCode).toBe(302)
+})
--- user.js
+++ user.js
@@ -1,7 +1,14 @@
-const mongoose = require("mongoose");
-const UserSchema = require("./userSchema");
+require('dotenv').config()
+const mongoose = require('mongoose')
+const UserSchema = require('./userSchema')
 
-mongoose.connect(process.env.DATABASE_URL || "mongodb://localhost/auth-test-dev", { autoIndex: false, useNewUrlParser: true });
-const db = mongoose.connection;
+mongoose.connect(
+  process.env.DATABASE_URL || 'mongodb://localhost/auth-test-dev',
+  { autoIndex: false, useUnifiedTopology: true, useNewUrlParser: true }
+)
 
-module.exports = mongoose.model("User", UserSchema);
+mongoose.connection.on('open', _ => {
+  console.log('Database is connected to', process.env.DATABASE_URL)
+})
+
+module.exports = mongoose.model('User', UserSchema)
